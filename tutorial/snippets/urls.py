f4e118d16ec7ba1dbd3e0b3d86b73f1c7de231f0
minh-atrihub <minhhn@atrihub.io>
2021-05-10 17:31:38 -0700

    feat(training): REST Tutorial
    Chapter 6: Viewsets & Routers
    
    [ICTRAINING-676]

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,16 +1,13 @@
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
-from snippets.views import SnippetList, SnippetDetail, UserList, UserDetail
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from snippets import views
 
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+# The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path('snippets/', SnippetList.as_view()),
-    path('snippets/<int:pk>/', SnippetDetail.as_view()),
-    path('users/', UserList.as_view()),
-    path('users/<int:pk>/', UserDetail.as_view()),
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
-urlpatterns += [
-    path('api-auth/', include('rest_framework.urls')),
+    path('', include(router.urls)),
 ]
